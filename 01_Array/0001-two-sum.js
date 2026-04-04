2faa268d009556327b2c96f67688bed1ad082014
Jorge-Leonell-G <jgrimaldog1900@alumno.ipn.mx>
2026-04-03 20:03:15 -0600

    (Array): two sum with array

--- 01_Array/0001-two-sum.js
+++ 01_Array/0001-two-sum.js
@@ -2,11 +2,11 @@
  * Problem: 0001. Two Sum
  * Difficulty: Easy
  * Category: Array, Hash Table
- * Daily: Yes
+ * Daily: No
  * Date: 02/04/2026
  * Link: https://leetcode.com/problems/two-sum/description/
- * Time Complexity: O(n)
- * Space Complexity: O(n)
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
  */
 
 /**
@@ -15,5 +15,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    //se procede a resolver con busqueda binaria
+    for (let i = 0; i < nums.length; i++){
+        for (let j = i + 1; j < nums.length; j++){
+            if (nums[i] + nums[j] === target){
+                return [i, j];
+            }
+
+        }
+    }
+    return [];
 };
